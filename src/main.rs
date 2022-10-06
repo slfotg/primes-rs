70da0374375a0f9920572bcf53af597a0bb3e8b8
Sam Foster <slfotg@gmail.com>
2022-10-06 11:28:04 -0500

    Improvements to naive sieve

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    let modulus = 10;
-    let max = 1000;
+    let modulus = 30;
+    let max = 10000000;
     let (tx, mut rx) = mpsc::channel(32);
-    for i in [2, 3, 5, 7, 9, 11] {
+    for i in [7, 11, 13, 17, 19, 23, 29, 31] {
         let tx2 = tx.clone();
         tokio::spawn(async move {
             let _ = tx2.send(get_primes(i, modulus, max)).await;
         });
     }
+    let _ = tx.send(vec![2, 3, 5]).await;
     drop(tx);
 
-    while (rx.recv().await).is_some() {
-        //println!("GOT = {}", message);
+    let mut size = 0;
+    while let Some(message) = rx.recv().await {
+        //println!("GOT = {:?}", message);
+        size += message.len();
     }
+    println!("Size = {}", size);
 }
 
 fn is_prime(p: u32) -> bool {
@@ -30,19 +33,18 @@ fn is_prime(p: u32) -> bool {
     true
 }
 
-fn get_primes(start: u32, delta: u32, max: u32) -> BitVec {
-    let size = (max / delta) - (start / delta);
+fn get_primes(start: u32, delta: u32, max: u32) -> Vec<u32> {
+    let size = (max / delta) - (start / delta) + u32::from((start % delta) <= max % delta);
+
     let mut vec = bitvec!(1; size as usize);
+
     for (i, mut e) in vec.iter_mut().enumerate() {
         let p = (i as u32) * delta + start;
         if !is_prime(p) {
             e.set(false);
         }
     }
-    let t: Vec<u32> = vec
-        .iter_ones()
+    vec.iter_ones()
         .map(|i| (i as u32) * delta + start)
-        .collect();
-    println!("{:?}", t);
-    vec
+        .collect()
 }
